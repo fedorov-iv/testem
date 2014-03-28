9e89c42b1678e88782ff9de625983818accf46d0
Fedorov Igor <i_fedorov@ifabrika.ru>
2014-03-28 17:57:13 +0400

    Кастомное сообщение в поле "Логин", если пользователь уже существует

--- registration/forms.py
+++ registration/forms.py
@@ -17,14 +17,15 @@ class RegistrationForm(forms.ModelForm):
             self.fields['last_name'] = forms.CharField(label=u"Фамилия", required=True)
             self.fields['email'] = forms.EmailField(label=u"E-mail", required=True)
 
-
-
         #пост-обработка данных формы с валидацией (оставил здесь, т.к. очень удобно в ряде случаев)
-        #def clean_email(self):
-            #form_email = self.cleaned_data.get('email')
-            #if form_email == '':
-                #raise forms.ValidationError("Это поле обязательно для заполнения.")
-            #return form_email
+        def clean_username(self):
+            form_username = self.cleaned_data.get('username')
+            try:
+                User.objects.filter(username=form_username)
+            except User.DoesNotExist:
+                return form_username
+            raise forms.ValidationError("Такое имя пользователя уже существует")
+
 
         #def clean_first_name(self):
             #form_first_name = self.cleaned_data.get('first_name')
