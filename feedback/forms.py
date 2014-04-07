dea4cc77e8eace580a641ac162ec52943151be4f
Fedorov Igor <i_fedorov@ifabrika.ru>
2014-04-07 19:06:38 +0400

    Обратная связь

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,9 +1,23 @@
 # -*- coding: utf8 -*-
 from feedback.models import Feedback
 from django import forms
+from django.contrib.auth. models import User
 
 
 class FeedbackForm(forms.ModelForm):
         class Meta:
             model = Feedback
             fields = ['subject', 'body', 'author_email']
+
+        def __init__(self, user=None, *args, **kwargs):
+            super(FeedbackForm, self).__init__(*args, **kwargs)
+            self._user = user
+
+        def clean_author_email(self):
+            if self._user.email:
+                #user = User.objects.get(username=self._user)
+                return self._user.email
+            elif self.cleaned_data.get('author_email'):
+                return self.cleaned_data.get('author_email')
+            else:
+                raise forms.ValidationError("Обязательное поле")
--- feedback/views.py
+++ feedback/views.py
@@ -9,7 +9,9 @@ def index(request):
     success = False
 
     if request.method == 'POST':
-        f = FeedbackForm(request.POST)
+
+        #user = request.user if request.user else None
+        f = FeedbackForm(user=request.user, data=request.POST)
         if f.is_valid():
             success = True
 
@@ -20,7 +22,7 @@ def index(request):
             # отправляем письмо с данными учетной записи пользователю
             mail_template = MailTemplate.objects.get(code__exact='USERFEEDBACK')
             body = mail_template.body.replace('###hostname###', request.get_host())
-            body = body.replace('###email###', request.POST.get('email'))
+            body = body.replace('###author_email###', f.instance.author_email)
             body = body.replace('###subject###', request.POST.get('subject'))
             body = body.replace('###body###', request.POST.get('body'))
 
@@ -28,7 +30,7 @@ def index(request):
                 mail_template.subject,
                 body,
                 mail_template.from_email,
-                [request.POST.get('email').strip(), ],
+                [mail_template.admin_email, ],
                 mail_template.copy_emails.split(';'),
                 headers={'Reply-To': mail_template.from_email, 'From': mail_template.from_name}
             )
@@ -41,4 +43,4 @@ def index(request):
         f = FeedbackForm()
 
     context = {'form': f, 'success': success}
-    return render(request, 'registration/index.html', context)
+    return render(request, 'feedback/index.html', context)
