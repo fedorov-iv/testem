4f1d8603cf04d5296d3bc4615c03855a402e1d98
Fedorov Igor <i_fedorov@ifabrika.ru>
2014-04-24 11:50:26 +0400

    Смена пароля (готово)

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -5,5 +5,6 @@ from myprofile import views
 urlpatterns = patterns('',
 
     url(r'^$', views.index, name='user_profile'),
+    url(r'^changepassword/$', views.change_password, name='change_password'),
 
 )
--- myprofile/views.py
+++ myprofile/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from myprofile.forms import MyProfileForm
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.core.urlresolvers import reverse
 
 @login_required
 def index(request):
@@ -11,7 +12,6 @@ def index(request):
     if request.method == 'POST':
         f = MyProfileForm(request.POST, instance=request.user)
         if f.is_valid():
-
             f.save()
 
             messages.add_message(request, messages.SUCCESS, "Изменения успешно сохранены!")
@@ -21,3 +21,13 @@ def index(request):
         f = MyProfileForm(instance=request.user)
 
     return render(request, "myprofile/user_profile.html", {'form': f})
+
+@login_required
+def change_password(request):
+
+    if request.method == 'POST':
+        request.user.set_password(request.POST.get('password'))
+        request.user.save()
+        messages.add_message(request, messages.SUCCESS, "Пароль успешно изменен!")
+
+    return redirect(reverse('user_profile'))
